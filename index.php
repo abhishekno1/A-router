8f1a7876c411cf3a4bfaab884cd39ce0170b0b63
abhishekno1 <abhishek.gurjar247@gmail.com>
2017-02-22 11:17:18 +0530

    new changes

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
 
-$uri = $_SERVER['REQUEST_URI'];
-
-$url = explode('?', $uri);
-
-$url = preg_replace('/\/router\//i', '', $url[0]);
-
 require "Bootstrap.php";
+
+$route = new RouteEngine();
+
+//$route -> add('test1','test1.php');
+//$route -> add('test3','test3.php');
+//$route -> add('name=abhishek','test3.php');
+$route -> map('tester/(\d+)', function($id){
+	echo "this will going to print".$id;
+});
+
+/*$route -> map('spider', function(){
+	echo "this is a seperate function I created to check the functionality of router";
+});*/
+//$route -> submit();
 ?>
