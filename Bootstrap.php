7b9c279458c5e3784f8cb2a993e199e6eb76def1
abhishekno1 <abhishek.gurjar247@gmail.com>
2017-02-22 11:21:25 +0530

    changes

--- Bootstrap.php
+++ Bootstrap.php
@@ -1,39 +1,98 @@
 <?php
 
-function get($var){
-	if($_GET[$var]){
-		$tvar = $_GET[$var];
-		$nvar = filter_var($tvar);
-		return $nvar;
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+class RouteEngine {
+
+	private $uri;
+	private $url;
+	private $queryString;
+	private $urlRewrite = array();
+	private $queryMatch = array();
+
+	public function __construct(){
+
+		$this->uri = $_SERVER['REQUEST_URI'];
+
+		$dec_url = urldecode($this->uri);
+
+		$this->url = explode('?', $dec_url);				
+
+		$this->queryString = $this->url[1];		
+
+		$this->url = preg_replace('/\/router\//i', '', $this->url[0]);		
+
 	}
-	else{
-		echo 'variable did\'nt exist';
+
+	//$url_sliced = implode( '/', $_GET['url'] );
+
+	//$noExe = array('.php','.html');
+
+	/**
+	* Adding Urls to targetted original urls
+	*
+	*
+	*/
+
+	public function add($tarUri,$target) {				
+
+		$this->urlRewrite[$tarUri] = $target;
+
+		$this->queryMatch[$tarUri] = $target;
+
 	}
+
+	//$find = in_array($url, $sofa);
+
+	/**
+	* Mapping function for urls
+	*
+	*
+	*/
+
+	public function map($tarUri, $funck) {
+		$res = preg_match("#$tarUri#", $this->url, $match);
+
+		if($res) {
+			$funck($match[1]);
+		}
+		if($this->url == $tarUri) {
+			$funck();
+		}
+		else if($this->queryString == $tarUri) {
+			$funck();
+		}
+		else
+			echo "404 NOT FOUND";						
+	}
+
+	/**
+	* Submit funtion for running the app
+	*
+	*
+	*/
+
+	public function submit() {						
+		//var_dump($this->url);
+		//var_dump($this->urlRewrite);
+
+		$this->find = array_key_exists($this->url, $this->urlRewrite);
+
+		if($this->find){						
+			$this->or_url = $this->urlRewrite[$this->url];
+			if(!empty($this->or_url))
+				require $this->or_url;
+			else
+				echo "404 NOT FOUND";
+		}
+		else{					
+			$this->or_url = $this->queryMatch[$this->queryString];
+			if(!empty($this->or_url))
+				require $this->or_url;
+			else
+				echo "404 NOT FOUND";
+		}
+	}
+
 }
-
-function post($var){
-	if($_POST[$var]){
-		$tvar = $_POST[$var];
-		$nvar = filter_var($tvar);
-		return $nvar;
-	}
-	else{
-		echo 'variable did\'nt exist';
-	}
-}
-
-//$url_sliced = implode( '/', $_GET['url'] );
-
-$sofa = array('test1/tester' => 'test1.php','test2' => 'test2.php','test3' => 'test3.php');
-
-//$find = in_array($url, $sofa);
-
-$find = array_key_exists($url, $sofa);
-
-if(isset($find)){
-	$or_url = $sofa[$url];
-
-	require $or_url;
-}
-
 ?>
